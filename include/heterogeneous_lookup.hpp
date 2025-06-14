2453210850d8f590e6508e81737d6601b31bc85c
Mikhail Zykov <zmsmihail@yandex.ru>
2025-06-14 23:24:18 +0300

    Fix after review

--- include/heterogeneous_lookup.hpp
+++ include/heterogeneous_lookup.hpp
@@ -4,21 +4,21 @@
 
 namespace bookdb {
 
-struct TransparentStringLess {
+struct TransparentTitleLess {
     using is_transparent = void;
     bool operator()(const std::string_view str, const Book &book) { return str < book.title; }
     bool operator()(const Book &book, const std::string_view str) { return book.title < str; }
     bool operator()(const Book &lhd, const Book &rhd) { return lhd.title < rhd.title; }
 };
 
-struct TransparentStringEqual {
+struct TransparentTitleEqual {
     using is_transparent = void;
     bool operator()(const std::string_view str, const Book &book) { return str == book.title; }
     bool operator()(const Book &book, const std::string_view str) { return book.title == str; }
     bool operator()(const Book &lhd, const Book &rhd) { return lhd.title == rhd.title; }
 };
 
-struct TransparentStringHash {
+struct TransparentTitleHash {
     using is_transparent = void;
     auto operator()(const Book &book) const { return std::hash<std::string>{}(book.title); }
     auto operator()(const std::string &title) const { return std::hash<std::string>{}(title); }
